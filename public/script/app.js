b846988a6dc1dc616d37a04c2eb32ef1aeb045da
shelvadim <shelgachev@yahoo.com>
2017-11-19 17:04:22 -0800

    Expressions

--- public/script/app.js
+++ public/script/app.js
@@ -5,11 +5,71 @@ console.log('app is runing');
 //JSX - JavaScript XML
 //babel src/app.js --out-file=public/script/app.js --presets=env,react --watch
 //live-server public
+var app = {
+  title: 'Hello',
+  subtitle: 'jsx'
+
+};
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'this is JSX change'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item2'
+    )
+  )
+);
+
+var user = {
+  name: 'Dora',
+  age: '24',
+  location: 'Rio'
+};
+
+/*var userName='Mike';
+var age=29;
+var loc='Richmond';*/
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Loc: ',
+    user.location
+  )
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -4,8 +4,40 @@ console.log('app is runing');
 //JSX - JavaScript XML
 //babel src/app.js --out-file=public/script/app.js --presets=env,react --watch
 //live-server public
+var app ={
+title:'Hello',
+subtitle:'jsx'
 
-var template = <p>this is JSX change</p>;
+};
+
+var template =(
+<div>
+  <h1>{app.title}</h1>
+  <p>{app.subtitle}</p>
+  <ol>
+    <li>Item1</li>
+    <li>Item2</li>
+  </ol>
+</div>
+);
+
+var user= {
+  name:'Dora',
+  age:'24',
+  location:'Rio'
+};
+
+/*var userName='Mike';
+var age=29;
+var loc='Richmond';*/
+
+var template2 =(
+<div>
+  <h1>{user.name}</h1>
+  <p>Age: {user.age}</p>
+  <p>Loc: {user.location}</p>
+</div>
+);
 
 var appRoot = document.getElementById('app');
 
