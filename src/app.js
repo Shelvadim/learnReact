ae76d018ca938d791f526b0bc6516bb669ae9cf1
shelvadim <shelgachev@yahoo.com>
2017-12-28 17:01:01 -0800

    test

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import {BrowserRouter,Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
+//
 const Page1=()=>(
     <div>
         Page1 component!
