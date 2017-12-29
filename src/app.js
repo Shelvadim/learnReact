ee332327bf17ce797a68d4c7d8c7f01ef588edd7
shelvadim <shelgachev@yahoo.com>
2017-12-28 17:22:32 -0800

    style 3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import {BrowserRouter,Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+//tetetetetetetetetete
+const Page1=()=>(
+    <div>
+        Page1 component!
+    </div>
+);
+
+const Page2=()=>(
+    <div>
+        Page2 component!!!!
+    </div>
+);
+
+const Page3=()=>(
+    <div>
+        Page3 component!
+    </div>
+);
+
+const Page4=()=>(
+    <div>
+        Page4 component!
+    </div>
+);
+
+
+const routes=(
+    <BrowserRouter>
+    <div>
+        <Route path="/" component={Page1} exact={true}/>
+        <Route path="/page2" component={Page2}/>
+        <Route path="/page3" component={Page3}/>
+        <Route path="/page4" component={Page4}/>
+    </div>    
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
 
