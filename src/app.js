7f5f4a452402bcfbe3f365683f664a180e4ccc38
shelvadim <shelgachev@yahoo.com>
2017-12-30 17:39:00 -0800

    test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
+//test
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
